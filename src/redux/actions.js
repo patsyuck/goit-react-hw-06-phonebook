5caa7762033b29c3c56dc871a84bbf435a049ae0
Oleksii Patsiuk <patsyuck@ukr.net>
2021-07-10 19:46:44 +0300

    Додав reducer

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,11 +1,14 @@
-export const addContact = {
+export const addContact = contact => ({
   type: 'ADD_CONTACT',
-};
+  payload: contact,
+});
 
-export const deleteContact = {
+export const deleteContact = id => ({
   type: 'DELETE_CONTACT',
-};
+  payload: id,
+});
 
-export const filterContacts = {
+export const filterContacts = event => ({
   type: 'FILTER_CONTACTS',
-};
+  payload: event.target.value,
+});
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,40 @@
 import { createStore } from 'redux';
 
-const reducer = (state = {}, action) => state;
+const initialState = {
+  contacts: [],
+  filter: '',
+};
+
+const reducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case 'ADD_CONTACT':
+      if (
+        state.contacts.some(
+          item => item.name.toLowerCase() === payload.name.toLowerCase(),
+        )
+      ) {
+        alert(`${payload.name} is already in contacts!`);
+        return state;
+      } else {
+        return {
+          contacts: [...state.contacts, payload],
+          filter: state.filter,
+        };
+      }
+    case 'DELETE_CONTACT':
+      return {
+        contacts: state.contacts.filter(item => item.id !== payload),
+        filter: state.filter,
+      };
+    case 'FILTER_CONTACTS':
+      return {
+        contacts: state.contacts,
+        filter: payload,
+      };
+    default:
+      return state;
+  }
+};
 
 const store = createStore(reducer);
 
